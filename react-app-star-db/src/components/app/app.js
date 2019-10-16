3b4b6efd104e033ee4483447024efe565a5c1f6e
kalinichika <viktoriya.v.kalinina@yandex.ru>
2019-10-16 17:50:42 +0300

    create React Application SratDB  about Star Wars

---
+++ react-app-star-db/src/components/app/app.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import Header from "../header";
+import ItemList from "../item-list";
+import RandomPlanet from "../random-planet";
+import PersonDetails from "../person-details";
+import "./app.css";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <RandomPlanet />
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
