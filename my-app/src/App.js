9d1f5aa97964c3f73011c0c99f00d8c15d567161
Vika <viktoriya.v.kalinina@yandex.ru>
2020-02-23 00:32:15 +0300

    Created a website for me

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import MainNavigation from "./components/Navigation";
+import MainContent from "./components/MainContent";
+import store from "./redux/store";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Provider store={store}>
+          <Router>
+            <MainNavigation/>
+            <MainContent/>
+          </Router>
+        </Provider>
+      </div>
+    );
+  }
 }
 
 export default App;
